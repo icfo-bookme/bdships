d4972d302bcb6d3938e62cd4dbd01265ef4bad47
icfo-bookem <nationalambulancebd@gmail.com>
2026-08-29 17:15:08 +0600

    gtm added

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import { SearchProvider } from "@/context/SearchContext";
 import { PaginationProvider } from "@/context/usePagination";
 import BookMeHeader from "@/components/shared/BookMeHeader/BookmeHeader";
+import Script from "next/script";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -27,8 +28,31 @@ export default function DashboardLayout({ children }) {
           crossOrigin="anonymous"
           referrerPolicy="no-referrer"
         />
+        {/* Google Tag Manager */}
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-K8P2P9G8');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
       </head>
       <body className={inter.className}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-K8P2P9G8"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <PaginationProvider>
           <SearchProvider>
               <div className="bg-white">
